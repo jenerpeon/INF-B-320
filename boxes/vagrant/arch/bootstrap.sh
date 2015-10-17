b0d0c2f059d5e52777f02e3e7cdf8e1503021c02
Lars Behrens <behrens_lars@gmx.de>
2015-10-17 21:03:11 +0000

    gitignore for tar.xz and vim swp, sample spring-maven project, bootstrap update

--- boxes/vagrant/arch/bootstrap.sh
+++ boxes/vagrant/arch/bootstrap.sh
@@ -1,21 +1,29 @@
 #!/usr/bin/env bash
 
-INTERNAL_PACKAGES="rsync vim git maven"
-EXTERNAL_PACKAGES=""
-RELEASE="arch"
-INSTALL="yaourt -Sue --noconfirm"
-INSTALL_SINGLE="yaourt -U"
-UPDATE="yaourt -Syy"
-INSTALL_STAGE1="pacman --noconfirm -S"
-UPDATE_STAGE1="pacman -Syy"
-PACKAGES_STAGE1="yaourt"
+LOCAL_SRC="/vagrant_data/packages/"
+ORACLE_JAVA="jdk-8u60-1-x86_64.pkg.tar.xz"
+INSTALL_SINGLE="pacman --noconfirm -U"
+INSTALL_PACKAGES="pacman --noconfirm -S"
+UPDATE_REPO="pacman -Syy"
+EXTERNAL_PACKAGES="rsync maven xorg-xauth eclipse-java"
 
-echo -e "[archlinuxfr]\nSigLevel = Optional TrustAll\nServer = http://repo.archlinux.fr/\$arch" >> /etc/pacman.conf
+echo -e "[archlinuxfr]\nSigLevel = Optional TrustAll\nServer = http://repo.archlinux.fr/\$arch"     >> /etc/pacman.conf
+eval "$UPDATE_REPO"
 
-eval $UPDATE_STAGE1
-[ -n $PACKAGES_STAGE1 ] && eval $INSTALL_STAGE1 $PACKAGES_STAGE1
+[ ! -e $LOCAL_SRC$ORACLE_JAVA ] \
+    && eval "$INSTALL_PACKAGES yaourt" \
+    && eval "yaourt -Syy" \
+    && eval "sudo -u vagrant yaourt -G jdk" \
+    && eval "cd jdk" \
+    && eval "sudo -u vagrant makepkg -si --noconfirm" \
+    && eval "mv $ORACLE_JAVA $LOCAL_SRC$ORACLE_JAVA" \
+    && eval "cd .. && rm -R jdk"
 
-eval $UPDATE
-[ -n $INTERNAL_PACKAGES ] && eval $INSTALL $INTERNAL_PACKAGES
+
+eval "$INSTALL_SINGLE $LOCAL_SRC$ORACLE_JAVA"
+
+[ -n "$EXTERNAL_PACKAGES" ] && eval "$INSTALL_PACKAGES $EXTERNAL_PACKAGES"
 
 exit 0
+
+
---
+++ boxes/vagrant/data/demo_project/src/main/java/example/WelcomeController.java
@@ -0,0 +1,14 @@
+package example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class WelcomeController {
+
+    @RequestMapping("/welcome") 
+    String welcome() {
+        return "Welcome"; 
+    }
+}
