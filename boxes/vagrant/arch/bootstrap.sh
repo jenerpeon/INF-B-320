6674a80455192db83c970ac7ae793bc4a2d05eeb
Lars Behrens <behrens_lars@gmx.de>
2015-10-18 17:50:22 +0000

    some fixes in the vagrant build

--- boxes/vagrant/arch/bootstrap.sh
+++ boxes/vagrant/arch/bootstrap.sh
@@ -5,23 +5,28 @@ ORACLE_JAVA="jdk-8u60-1-x86_64.pkg.tar.xz"
 INSTALL_SINGLE="pacman --noconfirm -U"
 INSTALL_PACKAGES="pacman --noconfirm -S"
 UPDATE_REPO="pacman -Syy"
+UPDATE_SYSTEM="pacman -Syu --noconfirm"
 EXTERNAL_PACKAGES="rsync maven xorg-xauth eclipse-java"
 
-echo -e "[archlinuxfr]\nSigLevel = Optional TrustAll\nServer = http://repo.archlinux.fr/\$arch"     >> /etc/pacman.conf
+echo -e "[archlinuxfr]\nSigLevel = Optional TrustAll\nServer = http://repo.archlinux.fr/\$arch">> /etc/pacman.conf
+
 eval "$UPDATE_REPO"
+eval "$UPDATE_SYSTEM"
 
-[ ! -e $LOCAL_SRC$ORACLE_JAVA ] \
-    && eval "$INSTALL_PACKAGES yaourt" \
-    && eval "yaourt -Syy" \
-    && eval "sudo -u vagrant yaourt -G jdk" \
-    && eval "cd jdk" \
-    && eval "sudo -u vagrant makepkg -si --noconfirm" \
-    && eval "mv $ORACLE_JAVA $LOCAL_SRC$ORACLE_JAVA" \
-    && eval "cd .. && rm -R jdk"
+if [ ! -e $LOCAL_SRC$ORACLE_JAVA ]; then
+    eval "$INSTALL_PACKAGES yaourt" 
+    yaourt -Syy
+    sudo -u vagrant yaourt -G jdk
+    cd /home/vagrant/jdk
+    sudo -u vagrant makepkg -si --noconfirm -p /home/vagrant/jdk/PKGBUILD
+    mv $ORACLE_JAVA $LOCAL_SRC$ORACLE_JAVA
+    cd .. && rm -R jdk
+fi
 
+eval "sudo chsh -s /bin/bash vagrant"
+eval "sudo -u vagrant touch /home/vagrant/.Xauthority"
 
 eval "$INSTALL_SINGLE $LOCAL_SRC$ORACLE_JAVA"
-
 [ -n "$EXTERNAL_PACKAGES" ] && eval "$INSTALL_PACKAGES $EXTERNAL_PACKAGES"
 
 exit 0
