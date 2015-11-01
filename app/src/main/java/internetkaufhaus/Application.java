85e0cdd8ee1f16aca8411a7e0b0672f18fe552a3
Lars Behrens <behrens_lars@gmx.de>
2015-11-01 20:31:29 +0100

    welcome implementation start

--- app/src/main/java/internetkaufhaus/Application.java
+++ app/src/main/java/internetkaufhaus/Application.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014.2015 the original author or authors.
+ * Copyright 2013-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,27 +17,58 @@ package internetkaufhaus;
 
 import org.salespointframework.EnableSalespoint;
 import org.salespointframework.SalespointSecurityConfiguration;
+import org.salespointframework.SalespointWebConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
+/**
+ * The central application class to configure the Spring container and run the application.
+ *
+ * @author Oliver Gierke
+ */
 @EnableSalespoint
 public class Application {
+
+	private static final String LOGIN_ROUTE = "/login";
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
 	@Configuration
+	static class VideoShopWebConfiguration extends SalespointWebConfiguration {
+
+		/**
+		 * We configure {@code /login} to be directly routed to the {@code login} template without any controller
+		 * interaction.
+		 * 
+		 * @see org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter#addViewControllers(org.springframework.web.servlet.config.annotation.ViewControllerRegistry)
+		 */
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addViewController(LOGIN_ROUTE).setViewName("login");
+		}
+	}
+
+	@Configuration
 	static class WebSecurityConfiguration extends SalespointSecurityConfiguration {
 
+		/**
+		 * Disabling Spring Security's CSRF support as we do not implement pre-flight request handling for the sake of
+		 * simplicity. Setting up basic security and defining login and logout options.
+		 * 
+		 * @see org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter#configure(org.springframework.security.config.annotation.web.builders.HttpSecurity)
+		 */
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
 
 			http.csrf().disable();
 
-			http.authorizeRequests().antMatchers("/**").permitAll().and().formLogin().loginProcessingUrl("/login").and()
-					.logout().logoutUrl("/logout").logoutSuccessUrl("/");
+			http.authorizeRequests().antMatchers("/**").permitAll().and().//
+					formLogin().loginPage(LOGIN_ROUTE).loginProcessingUrl(LOGIN_ROUTE).and(). //
+					logout().logoutUrl("/logout").logoutSuccessUrl("/");
 		}
 	}
 }
