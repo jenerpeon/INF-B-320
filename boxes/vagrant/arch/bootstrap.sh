dc24bf26f254cfbed2ea301fab642d5e7cafd8e2
Lars Behrens <behrens_lars@gmx.de>
2015-11-01 16:26:24 +0100

    kleine ueberarbeitungen der Akzeptanzfaell, Vagrant updates fuer vorkonfiguriertes eclipse

--- boxes/vagrant/arch/bootstrap.sh
+++ boxes/vagrant/arch/bootstrap.sh
@@ -6,7 +6,8 @@ INSTALL_SINGLE="pacman --noconfirm -U"
 INSTALL_PACKAGES="pacman --noconfirm -S"
 UPDATE_REPO="pacman -Syy"
 UPDATE_SYSTEM="pacman -Syu --noconfirm"
-EXTERNAL_PACKAGES="rsync maven xorg-xauth eclipse-java"
+EXTERNAL_PACKAGES="rsync maven xorg-xauth"
+PRECONFIGURED_ECLISE="/vagrant_data/packages"
 
 echo -e "[archlinuxfr]\nSigLevel = Optional TrustAll\nServer = http://repo.archlinux.fr/\$arch">> /etc/pacman.conf
 
@@ -34,6 +35,10 @@ eval "systemctl restart sshd"
 
 eval "$INSTALL_SINGLE $LOCAL_SRC$ORACLE_JAVA"
 [ -n "$EXTERNAL_PACKAGES" ] && eval "$INSTALL_PACKAGES $EXTERNAL_PACKAGES"
+
+[ -e $PRECONFIGURED_ECLIPSE ] && eval "tar xfvz $PRECONFIGURED_ECLIPSE -C /usr/lib/eclipse/"
+
+echo -e "export PATH=$PATH:/usr/lib/eclipse/" >> /home/vagrant/.bashrc
 
 exit 0
 
