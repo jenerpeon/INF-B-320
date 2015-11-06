677a23125be8c6958a608eaa5e218c2134be51c9
Linda <doanlinda93@gmail.com>
2015-11-06 15:17:08 +0100

    Anzeigen von Produkten noch Kategorie

--- app/src/main/java/internetkaufhaus/controller/CatalogController.java
+++ app/src/main/java/internetkaufhaus/controller/CatalogController.java
@@ -4,9 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.ModelMap;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import org.salespointframework.inventory.*;
 import internetkaufhaus.model.ConcreteProduct;
+import internetkaufhaus.model.ConcreteProduct.ProdType;
 import internetkaufhaus.model.ProductCatalog;
+
 
 @Controller
 public class CatalogController {
@@ -19,9 +26,23 @@ public class CatalogController {
     	this.inventory = inventory;
     }
     
-    @RequestMapping(value = {"/catalog"})
-    public String catalog(ModelMap modelmap){
-        modelmap.addAttribute("prodlist", catalog.findAll());	
+    @RequestMapping(value = {"/fuzzCatalog"})
+    public String fuzzcatalog(ModelMap modelmap){
+   
+        modelmap.addAttribute("catalog",catalog.findByType(ProdType.Fuzz));
+    	return "catalog";
+    }
+    
+    @RequestMapping(value = {"/trashCatalog"})
+    public String trashcatalog(ModelMap modelmap){
+   
+        modelmap.addAttribute("catalog",catalog.findByType(ProdType.Trash));
+    	return "catalog";
+    }
+    @RequestMapping(value = {"/garbageCatalog"})
+    public String garbagecatalog(ModelMap modelmap){
+   
+        modelmap.addAttribute("catalog",catalog.findByType(ProdType.Garbage));
     	return "catalog";
     }
 }
