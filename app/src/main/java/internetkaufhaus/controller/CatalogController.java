13822ac57855ea741ff9624268f11f292f9be1e0
Lars Behrens <behrens_lars@gmx.de>
2015-11-08 18:02:41 +0100

    generic catalogController

--- app/src/main/java/internetkaufhaus/controller/CatalogController.java
+++ app/src/main/java/internetkaufhaus/controller/CatalogController.java
@@ -1,13 +1,17 @@
 package internetkaufhaus.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.ModelMap;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 import org.salespointframework.inventory.*;
 import internetkaufhaus.model.ConcreteProduct;
@@ -19,11 +23,25 @@ import internetkaufhaus.model.ProductCatalog;
 public class CatalogController {
     private final ProductCatalog catalog;
     private final Inventory<InventoryItem> inventory;
+    private Map<ProdType, ArrayList<ConcreteProduct> > searchType = new HashMap<ProdType, ArrayList<ConcreteProduct> > ();
+    private List<ConcreteProduct> prods = new ArrayList<ConcreteProduct> ();
 
     @Autowired
     public CatalogController(ProductCatalog catalog, Inventory<InventoryItem> inventory){
     	this.catalog = catalog;
     	this.inventory = inventory;
+    	
+        for(ConcreteProduct prod : this.catalog.findAll()){
+            ProdType type = prod.getType();
+            ArrayList<ConcreteProduct> cache;
+            if(!this.searchType.containsKey(type))
+               this.searchType.put(type, new ArrayList<ConcreteProduct> ());
+            this.searchType.get(type).add(prod);
+        }
+    }
+    @RequestMapping(value = {"/catalog{type}"})
+    public String catalog(ModelMap modelmap, @PathVariable("type") String type ){
+    	
     }
     
     @RequestMapping(value = {"/fuzzCatalog"})
