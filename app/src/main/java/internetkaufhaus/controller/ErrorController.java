8a4ba45254c4a85b0182de6067227fadc1aab435
Max <max@xgme.de>
2015-11-20 00:06:44 +0100

    added error 500 page, modified the 404 page a little bit

--- app/src/main/java/internetkaufhaus/controller/ErrorController.java
+++ app/src/main/java/internetkaufhaus/controller/ErrorController.java
@@ -1,16 +1,19 @@
 package internetkaufhaus.controller;
 
+import java.io.IOException;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @Controller
 public class ErrorController {
-
 	@ExceptionHandler(RuntimeException.class)
-	public String error404(ModelMap model) {
-
-		return "error";
+	public String error404() {
+		return "error404";
 	}
 
+	@ExceptionHandler(IOException.class)
+	public String error500() {
+		return "error500";
+	}
 }
