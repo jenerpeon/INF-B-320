b7f0f1fabd126acb9be20107611c3780ce9a8c7e
Linda <doanlinda93@gmail.com>
2015-11-22 17:45:44 +0100

    controller change!

--- app/src/main/java/internetkaufhaus/controller/AdminController.java
+++ app/src/main/java/internetkaufhaus/controller/AdminController.java
@@ -8,10 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.salespointframework.SalespointSecurityConfiguration;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.validation.*;
 import internetkaufhaus.forms.*;
 import internetkaufhaus.model.*;
+import videoshop.model.CustomerRepository;
+
 import org.salespointframework.useraccount.*;
 import org.salespointframework.useraccount.web.LoggedIn;
 
@@ -20,20 +23,27 @@ import java.util.Optional;
 import javax.validation.*;
 import org.springframework.beans.factory.annotation.*;
 
-
+@Controller
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public class AdminController{
+	private final ConcreteUserAccountManager manager;
 
-//	@Autowired
-	public AdminController(){
+	@Autowired
+	public AdminController(ConcreteUserAccountManager manager){
+		this.manager=manager;	
+	}
+
+	@RequestMapping(value="/employee")
+	public String employees(ModelMap model){
+		model.addAttribute(manager.)
 		
 	}
 	
-	@RequestMapping(value="/userManagement")
+	
+	/*@RequestMapping(value="/userManagement")
 	public String userManagement(ModelMap model){
 		//model.addAttribute("customers", );
 		//model.addAttribute("admins",);
 		//model.addAttribute("employees",);
-	    return "index";
-	}
-	
+	    return "index";*/
 }
