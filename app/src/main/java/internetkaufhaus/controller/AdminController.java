40b4f664965938ed0a5e9485855dfb1c4bb32347
SoftMist <doanlinda93@gmail.com>
2015-11-23 17:04:02 +0100

    display customerlist

--- app/src/main/java/internetkaufhaus/controller/AdminController.java
+++ app/src/main/java/internetkaufhaus/controller/AdminController.java
@@ -28,6 +28,16 @@ public class AdminController{
 		
 	}
 	
+	@RequestMapping(value="/customer")
+	public String customers(ModelMap model){
+		Role role= Role.of("ROLE_COSTUMER");
+		model.addAttribute("customers", manager.findByRole(role)
+	
+		return "changecustomer";
+	}
+	
+	
+	
 	
 	/*@RequestMapping(value="/userManagement")
 	public String userManagement(ModelMap model){
