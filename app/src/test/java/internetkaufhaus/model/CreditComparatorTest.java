d851fe3c83fba068c1b91c2b658028ab9c59838f
Linda <doanlinda93@gmail.com>
2015-12-21 14:33:21 +0100

    Test for ConcreteUserAccount and CreditComparator

---
+++ app/src/test/java/internetkaufhaus/model/CreditComparatorTest.java
@@ -0,0 +1,53 @@
+package internetkaufhaus.model;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import static org.salespointframework.core.Currencies.EURO;
+import org.javamoney.moneta.Money;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.salespointframework.useraccount.Role;
+import org.salespointframework.useraccount.UserAccount;
+import org.salespointframework.useraccount.UserAccountManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+import internetkaufhaus.Application;
+import internetkaufhaus.entities.ConcreteUserAccount;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+
+public class CreditComparatorTest{
+	private ConcreteUserAccount model1;
+	private ConcreteUserAccount model2;
+	
+	@Autowired
+	CreditComparator comparator;
+	@Autowired
+	UserAccountManager u;
+	
+	
+	@Before
+	public void init(){
+	model1 = new ConcreteUserAccount("Username1", "Username1", Role.of("ROLE_CUSTOMER"), u);
+	model1.setCredits(Money.of(6, EURO));
+	model2 = new ConcreteUserAccount("test@mail.com", "Username2","Firstname", "Lastname", "Adress","ZipCode", "City", "Password", Role.of("ROLE_EMPLOYEE"),u);
+	model2.setCredits(Money.of(3, EURO));
+	}
+	
+	@Test
+	public void comparte(){
+	
+	assertTrue("compare",comparator.compare(model1, model2)==6);
+	}
+	
+	
+}
