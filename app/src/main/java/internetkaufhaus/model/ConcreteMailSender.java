ccc652a47f47724db18833ffffde759554ce8966
Heiner Ludwig <HeinerLudwig@Heiners-iMac.fritz.box>
2016-01-02 16:06:21 +0100

    javadoc MailSender

--- app/src/main/java/internetkaufhaus/model/ConcreteMailSender.java
+++ app/src/main/java/internetkaufhaus/model/ConcreteMailSender.java
@@ -10,14 +10,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Service("mailService")
 public class ConcreteMailSender {
 	
+	/**
+	 * This is the ConcreteMailSender class. It is responsible for sending mails via a connected mail provider.
+	 * 
+	 * @author heiner
+	 *
+	 */
+	
 	private final MailSender mailSender;
 	
 	@Autowired
 	public ConcreteMailSender(MailSender mailSender) {
 		this.mailSender = mailSender;
 	}
-
-	public void sendMail(String sendTo, String text, String from, String subject) {
+	
+	/**
+	 * The sendMail method will send a mail with the given mail attributes like the destination address or content. 
+	 * 
+	 * @param sendTo email destination address
+	 * @param text email text content
+	 * @param from sender name
+	 * @param subject email subject
+	 * 
+	 * @return <tt>true</tt> if mail sending was successful 
+	 * 
+	 * @exception MailException
+	 * 
+	 * @author heiner
+	 *
+	 */
+	
+	public boolean sendMail(String sendTo, String text, String from, String subject) {
 
 		SimpleMailMessage msg = new SimpleMailMessage();
 
@@ -28,8 +51,10 @@ public class ConcreteMailSender {
 
 		try {
 			mailSender.send(msg);
+			return true;
 		} catch (MailException ex) {
 			System.err.println(ex.getMessage());
+			return false;
 		}
 
 	}
